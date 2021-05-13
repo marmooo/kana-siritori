f3fd5aa0d314fb49f38967a0f1571523b7cb636d
marmooo <marmooo@users.noreply.github.com>
2021-05-13 10:47:31 +0900

    bootstrap5

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
-var CACHE_NAME = '2020-11-23 11:20';
+var CACHE_NAME = '2021-05-13 10:45';
 var urlsToCache = [
   '/kana-siritori/',
   '/kana-siritori/index.js',
   '/kana-siritori/index.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
