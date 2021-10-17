c590b0bbf230e875b16c2a8571a7ebd2f12fe2c2
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:46:56 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/kana-siritori/",
   "/kana-siritori/index.js",
   "/kana-siritori/siritori.json",
   "/kana-siritori/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
